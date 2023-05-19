4faae3cd08d5a3389fb93654dbdc23eb90399748
Ajeya <ajeyamd3@gmail.com>
2023-05-19 21:17:33 +0530

    Updated

---
+++ greatestof3.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter first number\n"))
+num2=int(input("Enter second number\n"))
+num3=int(input("Enter third number\n"))
+if (num1>=num2) and (num1>=num3):
+    print(f"{num1} is greatest")
+elif (num2>=num1) and (num2>num3):
+    print(f"{num2} is greatest")
+else:
+    print(f"{num3} is greatest")
+
+
