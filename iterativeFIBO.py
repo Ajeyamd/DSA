18a7fbdaabf47a58418ad190af7641d48d6efade
Ajeya <ajeyamd3@gmail.com>
2023-05-25 22:57:52 +0530

    updated

--- iterativeFIBO.py
+++ iterativeFIBO.py
@@ -1,6 +1,8 @@
-n = int(input("please give a number for fibonacci series : "))
+n = int(input("Please Enter the number for Fibonacci series : "))
 first,second=0,1
-print("fibonacci series are : ")
+print("Fibonacci series are : ")
+print("0")
+print("1")
 for i in range(0,n):
     if i<=1:
         result=i
@@ -8,4 +10,4 @@ for i in range(0,n):
       result = first + second;
       first = second;
       second = result;
-    print(result)
+      print(result)
