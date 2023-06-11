3909ce993e679d5f90c7e4dbcafb63ec84d3aa4b
Ajeya <ajeyamd3@gmail.com>
2023-06-11 19:58:12 +0530

    new

---
+++ reverseRow.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+newArr=[row[::-1] for row in arr]
+
+for i in range(len(newArr)):
+    for j in range(len(newArr[i])):
+        print(newArr[i][j],end =" ")
+    print()
+    
---
+++ rotate90.py
@@ -0,0 +1,20 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+ #Create a Null Matrix
+result=np.array([[0,0,0],[0,0,0],[0,0,0]])       
+
+#STEP1 TRANSPOSE (ROW -> COLUMN)
+for i in range(len(arr)):
+    for j in range(len(arr[0])):
+        result[j][i] = arr[i][j]
+
+
+#STEP 2 REVERSE THE ROW ELEMENTS
+result=[row[::-1] for row in result]
+
+for i in range(len(result)):
+    for j in range(len(result[0])):
+        print(result[i][j],end =" ")
+    print()
+    
