5a20b97bc693be1b9c0a43e33f5776c341d62a2e
Ajeya M D <ajeyamd3@gmail.com>
2023-08-17 21:38:31 +0530

    updated

--- assisgnment.py
+++ assisgnment.py
@@ -1,41 +1,47 @@
-def g(y):
-    b = 0
-    while y >= 3:
-        (y,b) = (y/3,b+1)
-    return(b)
+# def g(y):
+#     b = 0
+#     while y >= 3:
+#         (y,b) = (y/3,b+1)
+#     return(b)
 
-print(g(728))
+# print(g(728))
 
 
-def f(n):
-    s = 0
-    for i in range(2,n):
-        if n%i == 0 and i%2 == 1:
-            s = s+1
-    return(s)
+# def f(n):
+#     s = 0
+#     for i in range(2,n):
+#         if n%i == 0 and i%2 == 1:
+#             s = s+1
+#     return(s)
 
-print(f(90)-f(89))
+# print(f(90)-f(89))
 
-def h(n):
-    s = True
-    for i in range(1,n+1):
-        if i*i == n:
-            s = False
-    return(s)
+# def h(n):
+#     s = True
+#     for i in range(1,n+1):
+#         if i*i == n:
+#             s = False
+#     return(s)
 
-print(h(9))
+# print(h(9))
 
-startmsg = "python"
-endmsg = ""
-for i in range(1,1+len(startmsg)):
-  endmsg = startmsg[-i] + endmsg
+# startmsg = "python"
+# endmsg = ""
+# for i in range(1,1+len(startmsg)):
+#   endmsg = startmsg[-i] + endmsg
 
-print(endmsg)
+# print(endmsg)
+
+# def mystery(l):
+#   l = l[1:]
+#   return()
+
+# mylist = [7,11,13]
+# mystery(mylist)
+# print(mylist)
 
 def mystery(l):
-  l = l[1:]
-  return()
-
-mylist = [7,11,13]
-mystery(mylist)
-print(mylist)
+    if l == []:
+        return(l)
+    else:
+        return(l[-1:]+mystery(l[:-1]))
